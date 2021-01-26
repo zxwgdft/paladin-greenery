67d9cb316e92f1ff462055154cf123922bdd6fbe
Tonto <823498927@qq.com>
2021-01-26 15:53:32 +0800

    bug fix

--- organization/src/main/java/com/paladin/organization/config/OrganizationWebConfigurer.java
+++ organization/src/main/java/com/paladin/organization/config/OrganizationWebConfigurer.java
@@ -1,10 +1,12 @@
 package com.paladin.organization.config;
 
+import com.paladin.framework.spring.DevelopCondition;
 import com.paladin.framework.spring.web.DateFormatter;
 import com.paladin.organization.core.OrgSecurityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -19,10 +21,15 @@ public class OrganizationWebConfigurer implements WebMvcConfigurer {
     @Autowired
     private OrgSecurityManager webSecurityManager;
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        if (DevelopCondition.isDevelop(environment)) {
+            registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
     }
 
 
